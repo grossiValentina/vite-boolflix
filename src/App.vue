9b45eda8dfa094b8ec44a47f558fdc66a5dc4f88
grossiValentina <grossivalentina435@gmail.com>
2023-11-29 13:04:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
+import AppList from "./components/AppList.vue";
+import { store } from "./store.js";
+
+export default {
+  data() {
+    return {
+      store
+    };
+  },
+  components :{
+    AppHeader,
+    AppList,
+}
+}
+
+</script>
+
+<template>
+
+  <AppHeader />
+  <AppList />
+ 
+</template>
+
+<style lang="scss">
+@use "./components/style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+
+
+}
+</script>
+
+<template>
+
+    <div class="container">
+        <h1>BOOLFLIX</h1>
+        <div class="d-flex">
+            <input type="search" class="form-control" placeholder="cosa vuoi vedere" />
+            <button type="button" class="btn btn-dark">Cerca</button>
+        </div>
+    </div>
+    
+</template>
+
+<style scoped lang="scss"></style>
