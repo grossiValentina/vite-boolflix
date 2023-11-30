304718f30863f4aa206a9d1130f828e8c27972ad
grossiValentina <grossivalentina435@gmail.com>
2023-11-30 19:49:49 +0100

    add: esercizio wip

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -26,15 +26,15 @@ export default {
 <template>
     <div class="card" style="width: 15rem;">
 
-        <img class="img-cover" v-if="movieObject.poster_path" :src="`${imageCover}${movieObject.poster_path}`" alt="">
+        <img v-if="movieObject.poster_path" :src="`${imageCover}${movieObject.poster_path}`" alt="">
 
-        <img v-else class="w-100"
+        <img v-else
             src="https://c8.alamy.com/compit/2f8975j/nessun-simbolo-vettoriale-contorno-immagine-icona-mancante-nessuna-galleria-per-questo-momento-2f8975j.jpg"
             alt="">
 
-        <div class="card-body">
+         <div class="card-img-overlay text-center mt-4" style="width: 100%; height: 100%; position: absolute">
 
-            <h4 class="card-title">{{ movieObject.title ? movieObject.title : movieObject.name }}</h4>
+            <h4 class="overlay-text">{{ movieObject.title ? movieObject.title : movieObject.name }}</h4>
 
             <h5 class="card-title">{{ movieObject.original_title ? movieObject.original_title : movieObject.original_name }}
             </h5>
